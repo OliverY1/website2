ca0488a8b6c2e5a97c20a5c00d475a14c0b98f03
OliverY1 <166818916+OliverY1@users.noreply.github.com>
2024-04-13 16:24:57 +0200

    Update web.py

--- web.py
+++ web.py
@@ -9,8 +9,8 @@ st.set_page_config(page_title="BangBros", page_icon="random", layout="centered",
 
 hide_streamlit_style = """
             <style>
-            MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
+            [data-testid="stToolbar"] {visibility: hidden !important;}
+            footer {visibility: hidden !important;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
