5f585f9a0d6f8b31a106eb87f148a8c604ac90ec
OliverY1 <166818916+OliverY1@users.noreply.github.com>
2024-04-13 12:30:10 +0200

    Update web.py

--- web.py
+++ web.py
@@ -1,3 +1,4 @@
+import base64
 import streamlit as st
 from streamlit_option_menu import option_menu as op
 
@@ -12,18 +13,47 @@ with st.sidebar:
         default_index=0,
     )
 
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
 
 col1, col2, col3, col4 = st.columns([4, 4, 4, 4])
 
 if selected == "Hem":
+    set_background("bckg.png")
     st.title("BangBros")
     st.header("Stay tuned for more")
 
     with col1:
         st.image("Laok.jpg")
-        if st.button("Laok"):
+        if 'button' not in st.session_state:
+            st.session_state.button = False
+
+        def click_button():
+            st.session_state.button = not st.session_state.button
+
+        st.button('Laok', on_click=click_button)
+
+        if st.session_state.button:
+    
             st.write("Min favorit kurd. Laok är den klantigaste av dem alla. Han har en förmåga att kliva rakt in i situationer utan att tänka och orsakar oavsiktliga skador och olyckor vart han än går. Hans närvaro är som en ticking time bomb av klumpighet.")
     
+            
     with col2:
         st.image("Melle2.jpg")
         if st.button("Melvin"):
@@ -40,6 +70,6 @@ if selected == "Hem":
             st.write("Min favorit vattentank. Josef är den klumpiga medlemmen av gruppen. Han är ökänd för att ständigt stöta till saker, spilla saker och allmänt orsaka kaos runt omkring sig.")
 
 if selected == "Om oss":
+    set_background("bckg.png")
     st.title("Om oss")
     st.write("I en dold by, insvept i skogens tysta sånger och månens sken, samlas fem själar – Oliver, Laok, Melvin, Josef och Omar. Deras liv flätas samman av en gåtfull ödets tråd. Oliver bär en nyckel smidd av månens ljus, Laok har en gåva från jorden, Melvin bär solens strålar, Josef bär gryningens nyckel och Omar bär drömmarnas gåva. Deras gemensamma sökande tar dem genom dimmiga skogspassager och tätt lövverk, på jakt efter sanningen som vilar i det förlorade arvets skuggor.")
-
