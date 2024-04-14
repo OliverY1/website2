a18f9bd8a6ebb2233be532d5cdfea1553e8d47e2
OliverY1 <166818916+OliverY1@users.noreply.github.com>
2024-04-14 14:44:00 +0200

    Update web.py

--- web.py
+++ web.py
@@ -255,15 +255,14 @@ def dep():
 
 
 
-with st.sidebar:
-    selected = op(
-        menu_title="Meny",
+selected = op(
+        menu_title=None,
         options=["Hem", "Om oss", "Generator", "Blogg"],
         icons=["house-door-fill", "people-fill", "robot", "journal-text"],
         menu_icon="list",
         default_index=0,
-    )
-
+        orientation="horizontal",
+)
 def get_base64(bin_file):
     with open(bin_file, 'rb') as f:
         data = f.read()
