75384dd1d67792fcfb9bd90ddbc5254a9e10e39c
OliverY1 <166818916+OliverY1@users.noreply.github.com>
2024-04-23 13:36:50 +0200

    Update web.py

--- web.py
+++ web.py
@@ -3,9 +3,7 @@ import streamlit as st
 import random
 from streamlit_option_menu import option_menu as op
 
-st.set_page_config(page_title="BangBros", page_icon="random", layout="centered", initial_sidebar_state="auto", menu_items={
-    "Get help": "https://www.pornhub.com"
-},)
+st.set_page_config(page_title="BangBros", page_icon="random", layout="centered", initial_sidebar_state="auto")
 
 hide_streamlit_style = """
             <style>
@@ -291,34 +289,8 @@ col1, col2, col3, col4 = st.columns([4, 4, 4, 4])
 
 if selected == "Hem":
     set_background("bckg.png")
-    st.title("BangBros")
     st.header("Stay tuned for more")
 
-    with col1:
-        st.image("Laok.jpg")
-        if st.button("Laok"):
-            st.write("Min favorit kurd. Laok är den klantigaste av dem alla. Han har en förmåga att kliva rakt in i situationer utan att tänka och orsakar oavsiktliga skador och olyckor vart han än går. Hans närvaro är som en ticking time bomb av klumpighet.")
-    
-            
-    with col2:
-        st.image("Melle2.jpg")
-        if st.button("Melvin"):
-            st.write("Min favorit apa. Melvin är den drömmande idealisten i gruppen. Han har alltid sitt huvud i molnen och tror på det bästa i alla människor. Tyvärr gör hans naivitet honom till ett lätt byte för luriga typer.") 
-
-    with col3:
-        st.image("om.jpg")
-        if st.button("Omar"):
-            st.write("Min favorit sandätare. Omar är den som alltid tror sig ha lösningen på allt, men i själva verket är hans idéer vanligtvis helt bisarra och leder oftast till kaos. Han är den som alltid föreslår att de ska göra något extremt dumt bara för att det låter \"roligt\".")
-    
-    with col4:
-        st.image("jos.jpg")
-        if st.button("Josef"):
-            st.write("Min favorit vattentank. Josef är den klumpiga medlemmen av gruppen. Han är ökänd för att ständigt stöta till saker, spilla saker och allmänt orsaka kaos runt omkring sig.")
-
-if selected == "Om oss":
-    set_background("bckg.png")
-    st.title("Om oss")
-    st.write("I en dold by, insvept i skogens tysta sånger och månens sken, samlas fem själar – Oliver, Laok, Melvin, Josef och Omar. Deras liv flätas samman av en gåtfull ödets tråd. Oliver bär en nyckel smidd av månens ljus, Laok har en gåva från jorden, Melvin bär solens strålar, Josef bär gryningens nyckel och Omar bär drömmarnas gåva. Deras gemensamma sökande tar dem genom dimmiga skogspassager och tätt lövverk, på jakt efter sanningen som vilar i det förlorade arvets skuggor.")
 
 if selected == "Generator":
     set_background("bckg.png")
@@ -341,12 +313,3 @@ if selected == "Generator":
 
     if st.button("Motivation"):
         st.write(motivation())
-
-if selected == "Blogg":
-    set_background("note.jpg")
-    st.title("Blogg")
-    st.subheader("2024-04-14")
-    st.write("ww3 är här, jag ska joina Hamas nu. Länge leve all motstånd mot illegal ockupation, förtryck och folkmord. Må gud ha mercy på oss")
-    st.write("_________________________")
-    st.subheader("2024-04-13")
-    st.write("Idag har hemsidan lanserats!")
